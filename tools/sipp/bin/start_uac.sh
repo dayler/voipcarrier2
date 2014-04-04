bcd775c2a02b39718d99215c6ff19ccbeeabdfe4
Ariel Salazar <ariel.salazar@nuevatel.com>
2014-04-04 15:41:17 -0400

    Update tool/sipp scripts.

--- tools/sipp/bin/start_uac.sh
+++ tools/sipp/bin/start_uac.sh
@@ -1,44 +1,49 @@
 #!/bin/sh
 
-#Path for SIPP
-SIPP_PATH=/Users/lesly/development/sipp/3.3/bin/sipp
+# Path for SIPP
+SIPP_PATH=/usr/local/Cellar/sipp/3.3/bin/sipp
 
-#Path for descriptor
+# Path for descriptor
 UAS_DESC_PATH=descriptor/callee.xml
 UAC_DESC_PATH=descriptor/caller.xml
 
-#UAS port
+# UAS port
 UAS_PORT=5066
-#UAS Host
+
+# UAS Host
 UAS_HOST_IP=127.0.0.1
 
-#UAC port
+# UAC port
 UAC_PORT=5088
-#UAC Host
+
+# UAC Host
 UAC_HOST_IP=127.0.0.1
 
-#PROXY
+# PROXY
 PROXY_HOST_IP=127.0.0.1
 PROXY_PORT=5060
 
-#enable log = 1
+# Enable log = 1
 ACTIVE_LOG=1
 
-#External file with info
+# External file with info
 CSV_FILE=call.csv
 
-#Quantity of simultaneous calls
-L=10
+# Quantity of simultaneous calls
+L=2
 
-#Quantity of calls to be executed
-M=200
+# Quantity of calls to be executed
+M=5
+
+# Duration call, in milliseconds
+CALL_DELAY=30000
 
 if [ $ACTIVE_LOG  = 1 ];
 then
-    #input UAC
-    $SIPP_PATH -sf $UAC_DESC_PATH -p $UAC_PORT -i $UAC_HOST_IP -rsa $PROXY_HOST_IP:$PROXY_PORT $UAS_HOST_IP:$UAS_PORT -trace_err -trace_msg -inf $CSV_FILE -l $L -m $M
+    # Input UAC
+    $SIPP_PATH -sf $UAC_DESC_PATH -p $UAC_PORT -i $UAC_HOST_IP -rsa $PROXY_HOST_IP:$PROXY_PORT $UAS_HOST_IP:$UAS_PORT -trace_err -trace_msg -inf $CSV_FILE -l $L -m $M -d $CALL_DELAY
 else
-    #input UAC
-    $SIPP_PATH -sf $UAC_DESC_PATH -p $UAC_PORT -i $UAC_HOST_IP -rsa $PROXY_HOST_IP:$PROXY_PORT $UAS_HOST_IP:$UAS_PORT -inf $CSV_FILE -l $L -m $M
+    # Input UAC
+    $SIPP_PATH -sf $UAC_DESC_PATH -p $UAC_PORT -i $UAC_HOST_IP -rsa $PROXY_HOST_IP:$PROXY_PORT $UAS_HOST_IP:$UAS_PORT -inf $CSV_FILE -l $L -m $M -d $CALL_DELAY
 fi;
 exit
--- tools/sipp/bin/start_uas.sh
+++ tools/sipp/bin/start_uas.sh
@@ -1,44 +1,46 @@
 #!/bin/sh
 
 #Path for SIPP
-SIPP_PATH=/Users/lesly/development/sipp/3.3/bin/sipp
+SIPP_PATH=/usr/local/Cellar/sipp/3.3/bin/sipp
 
 #Path for descriptor
 UAS_DESC_PATH=descriptor/callee.xml
 UAC_DESC_PATH=descriptor/caller.xml
 
-#UAS port
+# UAS port
 UAS_PORT=5066
-#UAS Host
+
+# UAS Host
 UAS_HOST_IP=127.0.0.1
 
-#UAC port
+# UAC port
 UAC_PORT=5088
-#UAC Host
+
+# UAC Host
 UAC_HOST_IP=127.0.0.1
 
-#PROXY
+# PROXY
 PROXY_HOST_IP=127.0.0.1
 PROXY_PORT=5060
 
-#enable log = 1
+# Enable log = 1
 ACTIVE_LOG=1
 
-#External file with info
+# External file with info
 CSV_FILE=call.csv
 
-#Quantity of simultaneous calls
+# Quantity of simultaneous calls
 L=10
 
-#Quantity of calls to be executed
-M=200
+# Quantity of calls to be executed
+M=25
 
 if [ $ACTIVE_LOG  = 1 ];
 then
-    #input UAS
+    # Input UAS
     $SIPP_PATH -sf $UAS_DESC_PATH -p $UAS_PORT -i $UAS_HOST_IP  -rsa $PROXY_HOST_IP:$PROXY_PORT $UAC_HOST_IP:$UAC_PORT -trace_err -trace_msg -m $M
 else
-    #input UAS
+    # Input UAS
     $SIPP_PATH -sf $UAS_DESC_PATH -p $UAS_PORT -i $UAS_HOST_IP  -rsa $PROXY_HOST_IP:$PROXY_PORT $UAC_HOST_IP:$UAC_PORT -m $M
 fi;
 exit
